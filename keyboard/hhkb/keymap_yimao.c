feb237bb9be188fd0bf5e739333b3cbefaf24962
yimao <yimao@yimao.me>
2019-01-17 00:00:32 +0800

    Add mousekey and space&shift to keymap

--- keyboard/hhkb/keymap_yimao.c
+++ keyboard/hhkb/keymap_yimao.c
@@ -19,14 +19,15 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |-----------------------------------------------------------|
      * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn0|
      * `-----------------------------------------------------------'
-     *       |Fn0|Alt  |         Space         |Alt  |Gui|
+     *       |Fn0|Alt  |         SpaceMod      |Alt  |Gui|
      *       `-------------------------------------------'
      */
+    [0] = \
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
-           LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
+           LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   FN1, QUOT,ENT,             \
            LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0,             \
-                FN0,LALT,          SPC,                RALT,RGUI),
+                FN0,LALT,          FN4,                 RALT,RGUI),
 
     /* Layer 1: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
@@ -41,11 +42,34 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
      */
+    [1] = \
     KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,   \
            CAPS,MPRV,MPLY,MNXT,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, BSPC,      \
            TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,PENT,            \
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,            \
                 TRNS,TRNS,          TRNS,               TRNS,TRNS),
+
+    /* Layer 2: Mouse mode(IJKL)[Space]
+    * ,-----------------------------------------------------------.
+    * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
+    * |-----------------------------------------------------------|
+    * |Tab  |   |   |   |   |   |MwL|MwD|McU|MwU|MwR|Wbk|Wfr|Alt-T|
+    * |-----------------------------------------------------------|
+    * |Contro|   |   |   |   |   |Mb2|McL|McD|McR|Mb1|   |Return  |
+    * |-----------------------------------------------------------|
+    * |Shift   |   |   |   |   |Mb3|Mb2|Mb1|Mb4|Mb5|   |Shift |   |
+    * `-----------------------------------------------------------'
+    *      |    |Alt  |           Mb1          |Fn   |Fn |
+    *      `--------------------------------------------'
+    * Mc: Mouse Cursor / Mb: Mouse Button / Mw: Mouse Wheel
+    */
+    [2] = \
+    KEYMAP(GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
+           TAB, NO,  NO,  NO,  NO,  NO,  WH_L,WH_D,MS_U,WH_U,WH_R,FN2, FN3, BSPC,     \
+           LCTL,NO,  NO,  NO,  NO,  NO,  NO,  MS_L,MS_D,MS_R,FN1, NO,  ENT,           \
+           LSFT,NO,  NO,  NO,  NO,  BTN3,BTN2,BTN1,NO,  NO,  NO,  RSFT,TRNS,          \
+                TRNS,LALT,          BTN1,               TRNS,TRNS),
+
 };
 
 
@@ -55,11 +79,14 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
  */
 #ifdef KEYMAP_SECTION_ENABLE
 const action_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
-    [0]  = ACTION_LAYER_MOMENTARY(1),
-    [1]  = ACTION_LAYER_MOMENTARY(2),
-    [2]  = ACTION_LAYER_MOMENTARY(3),
-    [3]  = ACTION_LAYER_MOMENTARY(4),
-    [4]  = ACTION_LAYER_MOMENTARY(5),
+#else
+const action_t fn_actions[] PROGMEM = {
+#endif
+    [0]  = ACTION_LAYER_MOMENTARY(1),               // HHKB layer
+    [1]  = ACTION_LAYER_TAP_KEY(2, KC_SCLN),        // MouseKey layer with Semicolon
+    [2]  = ACTION_MODS_KEY(MOD_LALT, KC_LEFT),      // Walk back
+    [3]  = ACTION_MODS_KEY(MOD_LALT, KC_RIGHT),     // Walk forward
+    [4]  = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_SPACE), // Spance and shift
     [5]  = ACTION_LAYER_MOMENTARY(6),
     [6]  = ACTION_LAYER_MOMENTARY(7),
     [7]  = ACTION_LAYER_TOGGLE(1),
@@ -88,8 +115,3 @@ const action_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
     [30] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_GRAVE),
     [31] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_BSLASH),
 };
-#else
-const action_t fn_actions[] PROGMEM = {
-    [0]  = ACTION_LAYER_MOMENTARY(1),
-};
-#endif
