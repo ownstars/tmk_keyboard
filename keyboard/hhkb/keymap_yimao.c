fbb363d65def85f05ed7fab5293b60967b41e81f
yimao <yimao@yimao.me>
2019-02-10 11:09:36 +0800

    Add layer swith for keymap yimao, set layer without SaS as default

--- keyboard/hhkb/keymap_yimao.c
+++ keyboard/hhkb/keymap_yimao.c
@@ -17,19 +17,39 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |-----------------------------------------------------------|
      * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
      * |-----------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn0|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn5|
      * `-----------------------------------------------------------'
-     *       |Fn0|Alt  |         SpaceMod      |Alt  |Gui|
+     *       |Fn0|Alt  |         Space      |Alt  |Gui|
      *       `-------------------------------------------'
      */
     [0] = \
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
            LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   FN1, QUOT,ENT,             \
-           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0,             \
+           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN5,             \
+                FN0,LALT,          SPC,                 RALT,RGUI),
+
+    /* Layer 1: Default Layer With SpaceMod
+     * ,-----------------------------------------------------------.
+     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|
+     * |-----------------------------------------------------------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|
+     * |-----------------------------------------------------------|
+     * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
+     * |-----------------------------------------------------------|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |   |
+     * `-----------------------------------------------------------'
+     *       |Fn0|Alt  |         SpaceMod      |Alt  |Gui|
+     *       `-------------------------------------------'
+     */
+    [1] = \
+    KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
+           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
+           LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   FN1, QUOT,ENT,             \
+           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,TRNS,            \
                 FN0,LALT,          FN4,                 RALT,RGUI),
 
-    /* Layer 1: HHKB mode (HHKB Fn)
+    /* Layer 2: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
      * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
      * |-----------------------------------------------------------|
@@ -42,14 +62,14 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
      */
-    [1] = \
+    [2] = \
     KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,   \
            CAPS,MPRV,MPLY,MNXT,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, BSPC,      \
            TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,PENT,            \
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,            \
                 TRNS,TRNS,          TRNS,               TRNS,TRNS),
 
-    /* Layer 2: Mouse mode(IJKL)[Space]
+    /* Layer 3: Mouse mode(IJKL)[Space]
     * ,-----------------------------------------------------------.
     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
     * |-----------------------------------------------------------|
@@ -63,7 +83,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     *      `--------------------------------------------'
     * Mc: Mouse Cursor / Mb: Mouse Button / Mw: Mouse Wheel
     */
-    [2] = \
+    [3] = \
     KEYMAP(GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
            TAB, NO,  NO,  NO,  NO,  NO,  WH_L,WH_D,MS_U,WH_U,WH_R,FN2, FN3, BSPC,     \
            LCTL,NO,  NO,  NO,  NO,  NO,  NO,  MS_L,MS_D,MS_R,FN1, NO,  ENT,           \
@@ -82,12 +102,12 @@ const action_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
 #else
 const action_t fn_actions[] PROGMEM = {
 #endif
-    [0]  = ACTION_LAYER_MOMENTARY(1),               // HHKB layer
-    [1]  = ACTION_LAYER_TAP_KEY(2, KC_SCLN),        // MouseKey layer with Semicolon
-    [2]  = ACTION_MODS_KEY(MOD_LALT, KC_LEFT),      // Walk back
-    [3]  = ACTION_MODS_KEY(MOD_LALT, KC_RIGHT),     // Walk forward
-    [4]  = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_SPACE), // Spance and shift
-    [5]  = ACTION_LAYER_MOMENTARY(6),
+    [0]  = ACTION_LAYER_MOMENTARY(2),               // 0 HHKB layer-2
+    [1]  = ACTION_LAYER_TAP_KEY(3, KC_SCLN),        // 1 MouseKey layer with Semicolon
+    [2]  = ACTION_MODS_KEY(MOD_LALT, KC_LEFT),      // 2 Walk back
+    [3]  = ACTION_MODS_KEY(MOD_LALT, KC_RIGHT),     // 3 Walk forward
+    [4]  = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_SPACE), // 4 Spance and shift
+    [5]  = ACTION_LAYER_TOGGLE(1),                  // 5 Toggle layer-1
     [6]  = ACTION_LAYER_MOMENTARY(7),
     [7]  = ACTION_LAYER_TOGGLE(1),
     [8]  = ACTION_LAYER_TOGGLE(2),
